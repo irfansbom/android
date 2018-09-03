c19cd4343fa03d2dd48ac50c33c5a13e56394ead
Daniel <daniel.bailey@grappleIT.co.uk>
2018-09-03 20:15:21 +0100

    Dark theme v1
    Signed-off-by: Daniel Bailey <daniel.bailey@grappleIT.co.uk>

--- src/main/java/com/owncloud/android/db/PreferenceManager.java
+++ src/main/java/com/owncloud/android/db/PreferenceManager.java
@@ -66,6 +66,8 @@ public final class PreferenceManager {
     private static final String PREF__AUTO_UPLOAD_INIT = "autoUploadInit";
     private static final String PREF__FOLDER_SORT_ORDER = "folder_sort_order";
     private static final String PREF__FOLDER_LAYOUT = "folder_layout";
+
+    public static final String PREF__DARK_THEME = "darkTheme";
     public static final String PREF__LOCK_TIMESTAMP = "lock_timestamp";
 
     private PreferenceManager() {
@@ -472,6 +474,10 @@ public final class PreferenceManager {
      */
     public static void setAutoUploadSplitEntries(Context context, boolean splitOut) {
         saveBooleanPreference(context, PREF__AUTO_UPLOAD_SPLIT_OUT, splitOut);
+    }
+
+    public static boolean getTheme(Context context) {
+        return getDefaultSharedPreferences(context).getBoolean(PREF__DARK_THEME, false);
     }
 
     /**
